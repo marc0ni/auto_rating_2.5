6ebae668d925777565150d675087ef5ea5e88cf4
Mark Lindamood <mark.lindamood@yahoo.com>
2014-10-21 14:36:02 -0700

    Re-factored Submission model with 'models' array

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -7,39 +7,15 @@ class Submission < ActiveRecord::Base
   belongs_to :deductible
   belongs_to :limit
   
-  accepts_nested_attributes_for :proposals,
-      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
-      :allow_destroy => true
-      
-  accepts_nested_attributes_for :risks,
-      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
-      :allow_destroy => true
-      
-  accepts_nested_attributes_for :deductibles,
-      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
-      :allow_destroy => true
-      
-  accepts_nested_attributes_for :limits,
-          :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
-          :allow_destroy => true
+  models = [':proposals', ':risks', ':deductibles', ':limits']
   
+  accepts_nested_attributes_for { |models| models.all? { |l,m | }
+      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
+      :allow_destroy => true }
     
   def after_save
-    if self.proposals[0].name == ""
-      self.proposals[0].delete
+    if self.models[0].name == ""
+      self.models[0].delete
     end
-    
-    if self.risks[0].name == ""
-      self.risks[0].delete
-    end
-    
-    if self.deductibles[0].name == ""
-      self.deductibles[0].delete
-    end
-    
-    if self.limits[0].name == ""
-      self.limits[0].delete
-    end
-    
   end  
 end
