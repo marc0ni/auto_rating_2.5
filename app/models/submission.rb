2e6e273a5a362f4ec468a86d89e199ffb1b51438
Mark Lindamood <mark.lindamood@yahoo.com>
2014-10-20 21:21:32 -0700

    Modifying Submission object to allow nested attributes and 'after_save' method

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -6,4 +6,40 @@ class Submission < ActiveRecord::Base
   belongs_to :risk
   belongs_to :deductible
   belongs_to :limit
+  
+  accepts_nested_attributes_for :proposals,
+      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
+      :allow_destroy => true
+      
+  accepts_nested_attributes_for :risks,
+      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
+      :allow_destroy => true
+      
+  accepts_nested_attributes_for :deductibles,
+      :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
+      :allow_destroy => true
+      
+  accepts_nested_attributes_for :limits,
+          :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }, 
+          :allow_destroy => true
+  
+    
+  def after_save
+    if self.proposals[0].name == ""
+      self.proposals[0].delete
+    end
+    
+    if self.risks[0].name == ""
+      self.risks[0].delete
+    end
+    
+    if self.deductibles[0].name == ""
+      self.deductibles[0].delete
+    end
+    
+    if self.limits[0].name == ""
+      self.limits[0].delete
+    end
+    
+  end  
 end
